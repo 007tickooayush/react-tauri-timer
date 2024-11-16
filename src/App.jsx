7ad381ff2b02e15eba0bc9613abb789dc6a65c0c
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-16 23:43:35 +0530

    handling the input bar visibility

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 const defaultTime = 3;
-
+const leastTime = 0;
 function App() {
   const [active, setActive] = useState(false);
   const [time, setTime] = useState(defaultTime);
+
+  // useEffect(() => {
+  //   setActive(false);
+  //   setTime(defaultTime);
+  // }, []);
 
   useEffect(() => {
     let timer;
@@ -16,14 +21,18 @@ function App() {
         setTime(prevTime => prevTime - 1);
       }, 1000);
     }
+    if(time === leastTime) {
+      setActive(false);
+    }
 
-    return () => clearInterval(timer);
+    console.log("Time: ", time, "Active: ", active);
+    return () => {
+      clearInterval(timer);
+    };
   }, [active, time]);
 
   const handleTimeInit = (value) => {
-    // if(!active) {
-      setTime(value);
-    
+    setTime(value);
   };
 
   const handleStart = () => {
@@ -43,11 +52,14 @@ function App() {
     <main className="container">
       <h1>Tauri App Mark1</h1>
       <div className="container">
-        <input 
-          type="number" 
-          onChange={(e) => handleTimeInit(Number(e.target.value))} 
-          value={time} 
-        />
+        {
+          !active &&
+          <input 
+            type="number" 
+            onChange={(e) => handleTimeInit(Number(e.target.value))} 
+            value={time} 
+          />
+        }
         <div className="row" style={{padding: 4, margin: 4}}>
           <button onClick={handleStart} style={{margin: 4}}>Start</button>
           <button onClick={handleStop} style={{margin: 4}}>Stop</button>
@@ -62,10 +74,3 @@ function App() {
 }
 
 export default App;
-/**
- *      <input 
-          type="number" 
-          onChange={(e) => handleTimeInit(Number(e.target.value))} 
-          value={time} 
-        />
- */
