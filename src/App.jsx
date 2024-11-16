b190bfc5ea464961eb12638f879a7adaa169d219
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-16 23:25:31 +0530

    start stop and reset implementation

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,58 @@ function App() {
     if(active && time > 0) {
       timer = setInterval(() => {
         setTime(prevTime => prevTime - 1);
-      });
+      }, 1000);
     }
+
     return () => clearInterval(timer);
   }, [active, time]);
 
+  const handleTimeInit = (value) => {
+    // if(!active) {
+      setTime(value);
+    
+  };
+
+  const handleStart = () => {
+    setActive(true);
+  };
+
+  const handleStop = () => {
+    setActive(false);
+  };
+
+  const handleReset = () => {
+    setTime(defaultTime);
+    setActive(false);
+  };
+
   return (
-    <main class="container">
+    <main className="container">
       <h1>Tauri App Mark1</h1>
+      <div className="container">
+        <input 
+          type="number" 
+          onChange={(e) => handleTimeInit(Number(e.target.value))} 
+          value={time} 
+        />
+        <div className="row" style={{padding: 4, margin: 4}}>
+          <button onClick={handleStart} style={{margin: 4}}>Start</button>
+          <button onClick={handleStop} style={{margin: 4}}>Stop</button>
+          <button onClick={handleReset} style={{margin: 4}}>Reset</button>
+        </div>
+      </div>
+      <h2>
+        {time}
+      </h2>
     </main>
   );
 }
 
 export default App;
+/**
+ *      <input 
+          type="number" 
+          onChange={(e) => handleTimeInit(Number(e.target.value))} 
+          value={time} 
+        />
+ */
