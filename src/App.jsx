10bb9ad278365aceb6da2afc2d83e496c28fad6a
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-16 23:03:54 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <h1>Tauri App Mark1</h1>
+  );
+}
+
+export default App;
