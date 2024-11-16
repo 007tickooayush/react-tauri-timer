f349d00b56c7f36da7dfbcb64ba9f9f0b780ed3f
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-16 23:10:18 +0530

    added the eleemnts

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
+const defaultTime = 3;
+
 function App() {
+  const [active, setActive] = useState(false);
+  const [time, setTime] = useState(defaultTime);
 
   useEffect(() => {
-
-  }, []);
+    let timer;
+    if(active && time > 0) {
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+      });
+    }
+    return () => clearInterval(timer);
+  }, [active, time]);
 
   return (
-    <h1>Tauri App Mark1</h1>
+    <main class="container">
+      <h1>Tauri App Mark1</h1>
+    </main>
   );
 }
 
